863df4db17b960afd1205e4c7dbe5555e982f49b
JhonSmith-1987 <77317038+JhonSmith-1987@users.noreply.github.com>
2024-11-28 23:46:09 -0500

    change config sequelize

--- src/infrastructure/config/sequelizeConfig.ts
+++ src/infrastructure/config/sequelizeConfig.ts
@@ -3,13 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true, // Render generalmente requiere SSL
-            rejectUnauthorized: false, // Permite certificados autofirmados
-        },
-    },
-});
+export const sequelize = new Sequelize(
+    process.env.DB_DATABASE || 'pos_db',
+    process.env.DB_USER || 'fresa',
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres'
+    }
+);
+
+// export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+//     dialect: 'postgres',
+//     protocol: 'postgres',
+//     dialectOptions: {
+//         ssl: {
+//             require: true, // Render generalmente requiere SSL
+//             rejectUnauthorized: false, // Permite certificados autofirmados
+//         },
+//     },
+// });
